78b1f39bf22cdb0dc93edd4115ff39d17ba4b999
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 21:25:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -56,26 +56,74 @@ array = ["Tom Frank", "Cuchadooks Kelley", "Potato Chip"]
 
 
 #collection = ["thing 1", "thing 2", "Thing 3"]
-def my_collect(collection)
-  i= 0
-  new_array =[]
-  while (i < collection.length) 
-#  yield collection[i] 
-#    if collection[1] == collection[1].capitalize
-#      puts collection + collection.capitalize
-#      new_array << collection[i].split(" ").first
-#    else
-#      new_array << collection[i].upcase
-#    end
+#def my_collect(collection)
+#  i= 0
+#  new_array =[]
+#  while (i < collection.length) 
+##  yield collection[i] 
+##    if collection[1] == collection[1].capitalize
+##      puts collection + collection.capitalize
+##      new_array << collection[i].split(" ").first
+##    else
+##      new_array << collection[i].upcase
+##    end
 ######
-  yield collection[i]
-  i += 1
-  end
- new_array
-end
+#  yield collection[i]
+#  i += 1
+#  end
+#new_array
+#end
 
 
 #my_collect(collection) { |name| name.split(" ").first }
 
-my_collect { |item| new_array << item }
+#my_collect { |item| new_array << item }
+
+
+
+#def yielding_with_arguments(num)
+#  puts "the program is executing the code inside the method"
+#  yield(27)
+#  puts "now we are back in the method"
+#end
+
+#yielding_with_arguments(64) {|i| puts i * 2}
+
+###Example below of the starter yield block example
+
+#def hello_t(array)
+#  if block_given?
+#    i = 0
+#    while i < array.length
+#      yield array[i]
+#      i += 1
+#    end
+#    array
+#  else
+#    puts "Hey! No block was given!"
+#  end
+#end
+
+#array= ["Tim", "Tom", "Jim"]
+
+#hello_t(array) do |name|
+#  if name.start_with?("T")
+#    puts "Hi, #{name}"
+#  end
+#end
+
+
+def my_collect(array)
+  i = 0
+  while i < array.length
+    yield array[i]
+    i += 1
+  end
+end  
+  
+  
+my_collect(array) do |name|
+  name.split(" ").first 
+  end
+end
 
