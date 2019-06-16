69e630942e83f95ecae96fffaf105806ea87b56d
Ursula Miller <ursula.miller@gmail.com>
2019-06-16 21:28:00 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -117,8 +117,8 @@ def my_collect(array)
   i = 0
   new_array= []
   while i < array.length
-    yield array[i]
-    new_array << name
+    
+    new_array << yield array[i]
     i += 1
   end
   new_array 
